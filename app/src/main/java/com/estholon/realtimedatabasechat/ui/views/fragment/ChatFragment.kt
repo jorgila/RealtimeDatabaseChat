cd88ab4a4593287d0b4aff0bcbcb740b12a3cab0
Jorge Gimeno <jgimenolatas@gmail.com>
2024-03-02 09:31:32 +0100

    "Chat Items"

--- app/src/main/java/com/estholon/realtimedatabasechat/ui/views/fragment/ChatFragment.kt
+++ app/src/main/java/com/estholon/realtimedatabasechat/ui/views/fragment/ChatFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.estholon.realtimedatabasechat.R
 import com.estholon.realtimedatabasechat.databinding.FragmentChatBinding
 
@@ -22,9 +23,15 @@ class ChatFragment : Fragment() {
         // BINDING START
         binding = FragmentChatBinding.inflate(inflater,container,false)
         // BINDING BODY
-
+        initListeners()
         // BINDING END
         return binding.root
+    }
+
+    private fun initListeners() {
+        binding.ivBack.setOnClickListener {
+            findNavController().navigate(R.id.action_chat_fragment_to_main_fragment)
+        }
     }
 
 }
--- app/src/main/java/com/estholon/realtimedatabasechat/ui/views/fragment/MainFragment.kt
+++ app/src/main/java/com/estholon/realtimedatabasechat/ui/views/fragment/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.estholon.realtimedatabasechat.R
 import com.estholon.realtimedatabasechat.databinding.FragmentMainBinding
 
@@ -19,9 +20,17 @@ class MainFragment : Fragment() {
         // BINDING START
         binding = FragmentMainBinding.inflate(inflater,container,false)
         // BINDING BODY
-
+        initListeners()
         // BINDING END
         return binding.root
+    }
+
+    private fun initListeners() {
+        binding.btnChat.setOnClickListener {
+            if(!binding.tieName.text.isNullOrEmpty()){
+            findNavController().navigate(R.id.action_main_fragment_to_chat_fragment)
+            }
+        }
     }
 
 }
